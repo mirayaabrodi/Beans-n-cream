de666c3247602432bf50f367c3c64f4c05040876
mirayaabrodi <mirayaabrodi@gmail.com>
2020-12-25 13:31:41 +0100

    feat(beans-n-cafe): first commit and push to master

---
+++ contact/contact.js
@@ -0,0 +1,31 @@
+function validation() {
+  var form = document.getElementById('form');
+  var email = document.getElementById('email').value;
+  var text = document.getElementById('text');
+  var pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+
+  if (email.match(pattern))
+  {
+    form.classList.add('valid');
+    form.classList.remove('invalid');
+    text.innerHTML = 'Your Email Adress is Valid';
+    text.style.color = '#00ff00';
+  }
+
+else (email.match(pattern))
+  {
+    form.classList.remove('valid');
+    form.classList.add('invalid');
+    text.innerHTML = 'Please Enter Valid Email Address';
+    text.style.color = '#ff0000';
+  }
+
+   
+ if (email == '')
+  {
+    form.classList.remove('valid');
+    form.classList.remove('invalid');
+    text.innerHTML = '';
+    text.style.color = '#00ff00';
+  } 
+}
